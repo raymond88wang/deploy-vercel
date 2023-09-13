d7f6fc1d8e2afd66f0e3ca8e3cbda827f18af7f2
raymond88wang <raymond88wang@gmail.com>
2023-09-12 21:21:15 -0700

    change to vercel

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => { console.log('connected to mongo: ', process.env.MONGO_URI) })
+  .connect(process.env.MONGODB_URI)
+  .then(() => { console.log('connected to mongo: ', process.env.MONGODB_URI) })
 
 
 // MIDDLEWARE
