b3535db33260051295d3d353ccdaf93d37a886c2
raymond88wang <raymond88wang@gmail.com>
2023-09-12 21:23:59 -0700

    test

--- server.js
+++ server.js
@@ -12,31 +12,31 @@ mongoose
   .then(() => { console.log('connected to mongo: ', process.env.MONGODB_URI) })
 
 
-// MIDDLEWARE
-// app.use(methodOverride('_method'))
-// app.use(express.static('public'))
-// app.use(express.urlencoded({extended: true}))
-// app.set('views', __dirname + '/views')
-// app.set('view engine', 'jsx')
-// app.engine('jsx', require('express-react-views').createEngine())
+MIDDLEWARE
+app.use(methodOverride('_method'))
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
 
 // ROUTES
 app.get('/', (req, res) => {
   res.send('Welcome to an Awesome App about Breads')
 })
   
-// // Breads
-// const breadsController = require('./controllers/breads_controller.js')
-// app.use('/breads', breadsController)
+// Breads
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
 
-// // Bakers
-// const bakersController = require('./controllers/bakers_controller.js')
-// app.use('/bakers', bakersController)
+// Bakers
+const bakersController = require('./controllers/bakers_controller.js')
+app.use('/bakers', bakersController)
 
-// // 404 Page
-// app.get('*', (req, res) => {
-//   res.render('NotFound')
-// })
+// 404 Page
+app.get('*', (req, res) => {
+  res.render('NotFound')
+})
 
 // LISTEN
 app.listen(PORT, () => {
