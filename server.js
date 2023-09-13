04242c43d1c08cee8ffda53e2a6876b286f2d023
raymond88wang <raymond88wang@gmail.com>
2023-09-12 19:03:37 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+require('dotenv')
+const app = express()
+
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!')
+})
+
+app.listen(process.env.PORT)
+
+module.exports = app
